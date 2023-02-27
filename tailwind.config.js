06e2e74a7329611127401ea141cdfbc45accf1a8
Your Name <you@example.com>
2023-02-27 18:42:14 +0100

    tailwind container config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./src/**/*.js'],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+      },
+    },
   },
   plugins: [],
 }
