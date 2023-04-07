a0e687e3af3f96aa7700f588a83cdab5654c8517
suki60 <fran.altes@gmail.com>
2023-04-07 17:51:08 +0200

    ThemeProvider

---
+++ src/contexts/ThemeContext.js
@@ -0,0 +1,13 @@
+import { createContext, useContext } from "react"
+import resolveConfig from "tailwindcss/resolveConfig"
+import tailwindConfig from "tailwind.config"
+
+const ThemeContext = createContext()
+
+export const useTheme = () => useContext(ThemeContext)
+
+export const ThemeProvider = ({ children }) => {
+  const theme = resolveConfig(tailwindConfig).theme
+
+  return <ThemeContext.Provider value={theme}>{children}</ThemeContext.Provider>
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,16 @@
 import Layout from "@/components/Layout"
+import { ThemeProvider } from "@/contexts/ThemeContext"
 import { ViewportProvider } from "@/contexts/ViewportContext"
 import "@/styles/globals.css"
 
 export default function App({ Component, pageProps }) {
   return (
-    <ViewportProvider ssrViewport="lg">
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ViewportProvider>
+    <ThemeProvider>
+      <ViewportProvider ssrViewport="lg">
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ViewportProvider>
+    </ThemeProvider>
   )
 }
