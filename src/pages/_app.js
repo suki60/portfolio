d663676197304eb1aa9f53dab20d492b504186c5
Your Name <you@example.com>
2023-04-21 11:58:37 +0200

    getSSRViewport logic

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,37 @@
+import App from "next/app"
 import Layout from "@/components/Layout"
 import { ThemeProvider } from "@/contexts/ThemeContext"
 import { ViewportProvider } from "@/contexts/ViewportContext"
+import UAParser from "ua-parser-js"
 import "@/styles/globals.css"
 
-export default function App({ Component, pageProps }) {
-  return (
-    <ThemeProvider>
-      <ViewportProvider ssrViewport="lg">
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ViewportProvider>
-    </ThemeProvider>
-  )
+const getSSRViewport = (userAgent) => {
+  const ua = UAParser(userAgent)
+  const deviceType = ua.device.type
+
+  if (deviceType === "mobile") return "xs"
+
+  if (deviceType === "tablet") return "sm"
+
+  return "lg"
 }
+
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider>
+    <ViewportProvider ssrViewport={pageProps.ssrViewport}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ViewportProvider>
+  </ThemeProvider>
+)
+
+MyApp.getInitialProps = async (ctx) => {
+  const props = await App.getInitialProps(ctx)
+  const ssrViewport = getSSRViewport(ctx.ctx.req.headers["user-agent"])
+  props.pageProps.ssrViewport = ssrViewport
+
+  return { ...props }
+}
+
+export default MyApp
