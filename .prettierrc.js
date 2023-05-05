96935df7741fd821176fa61c7e041da5d88e33ea
suki60 <fran.altes@gmail.com>
2023-05-05 15:45:22 +0200

    add prettier conf

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+  printWidth: 120,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  useTabs: false,
+  arrowParens: 'avoid',
+}
