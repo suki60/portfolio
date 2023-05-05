50a3412cb93ad6e1f3da2f9bc80f93455636cee6
suki60 <fran.altes@gmail.com>
2023-05-05 15:47:49 +0200

    minimal menu burger

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,59 @@
 import { useViewport } from '@/contexts/ViewportContext'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
+
+const Menu = ({ open }) => {
+  return (
+    <nav
+      className={`flex transform flex-col justify-center bg-green-100 ${
+        open ? 'translate-x-0' : '-translate-x-full'
+      } absolute top-0 left-0 h-screen px-8 py-8 text-left transition-transform duration-300 ${open ? 'z-50' : '-z-1'}`}
+    >
+      <Link href='/'>home</Link>
+      <Link href='contact'>contact</Link>
+      <Link
+        className='rounded border border-black py-1 px-2 text-black hover:bg-black hover:text-white'
+        href='https://drive.google.com/file/d/1zBiam17FE5C2ZuUv4dCdp0Lo3Y6_UEvG/view?usp=sharing'
+        target='_blank'
+        role='button'
+      >
+        résumé
+      </Link>
+    </nav>
+  )
+}
+
+const Burger = ({ open, setOpen }) => {
+  return (
+    <button className='z-50 flex h-8 w-8 flex-col justify-around p-0' onClick={() => setOpen(!open)}>
+      <div
+        className={`h-1 w-full origin-center transform rounded-full bg-gray-800 transition-all duration-300 ${
+          open ? ' rotate-45' : 'rotate-0'
+        }`}
+        style={{ transformOrigin: '1px center' }}
+      />
+      <div
+        className={`h-1 w-full transform rounded-full bg-gray-800 transition-all duration-300 ${
+          open ? 'opacity-0' : 'opacity-100'
+        } ${open ? 'translate-x-20' : 'translate-x-0'}`}
+      />
+      <div
+        className={`h-1 w-full origin-center transform rounded-full bg-gray-800 transition-all duration-300 ${
+          open ? '-rotate-45' : 'rotate-0'
+        }`}
+        style={{ transformOrigin: '1px center' }}
+      />
+    </button>
+  )
+}
 
 const Navbar = ({}) => {
   const { up } = useViewport()
+  const [open, setOpen] = useState(false)
 
   return (
-    <div className='flex justify-between items-center px-6 h-[var(--navbar-height)]'>
+    <div className='flex h-[var(--navbar-height)] items-center justify-between px-6'>
       <Link href='/'>
         <Image alt='logo' src='/fa.svg' height={30} width={30} />
       </Link>
@@ -15,17 +61,20 @@ const Navbar = ({}) => {
         <div className='flex items-center gap-6'>
           <Link href='/'>home</Link>
           <Link href='contact'>contact</Link>
-          <a
-            className='hover:bg-black text-black hover:text-white py-1 px-2 border border-black rounded'
+          <Link
+            className='rounded border border-black py-1 px-2 text-black hover:bg-black hover:text-white'
             href='https://drive.google.com/file/d/1zBiam17FE5C2ZuUv4dCdp0Lo3Y6_UEvG/view?usp=sharing'
             target='_blank'
             role='button'
           >
             résumé
-          </a>
+          </Link>
         </div>
       ) : (
-        <div>burger</div>
+        <>
+          <Menu open={open} />
+          <Burger open={open} setOpen={setOpen} />
+        </>
       )}
     </div>
   )
