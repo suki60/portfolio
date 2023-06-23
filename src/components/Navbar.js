485816359339ed211850b38f5044c088443cf06d
Your Name <you@example.com>
2023-06-23 12:04:01 +0200

    extract logic to Burger component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React, { useState } from 'react'
 import Button from './Button'
+import Burger from './Burger'
 
 const Menu = ({ open }) => {
   return (
@@ -17,30 +18,6 @@ const Menu = ({ open }) => {
         <Button />
       </div>
     </nav>
-  )
-}
-
-const Burger = ({ open, setOpen }) => {
-  return (
-    <button className='z-50 flex h-8 w-8 flex-col justify-around p-0' onClick={() => setOpen(!open)}>
-      <div
-        className={`h-1 w-full origin-center transform rounded-full bg-black transition-all duration-300 ${
-          open ? ' rotate-45' : 'rotate-0'
-        }`}
-        style={{ transformOrigin: '1px center' }}
-      />
-      <div
-        className={`h-1 w-full transform rounded-full bg-black transition-all duration-300 ${
-          open ? 'opacity-0' : 'opacity-100'
-        } ${open ? 'translate-x-20' : 'translate-x-0'}`}
-      />
-      <div
-        className={`h-1 w-full origin-center transform rounded-full bg-black transition-all duration-300 ${
-          open ? '-rotate-45' : 'rotate-0'
-        }`}
-        style={{ transformOrigin: '1px center' }}
-      />
-    </button>
   )
 }
 
