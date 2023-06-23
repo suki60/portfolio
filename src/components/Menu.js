86ae51b8d50b75e3db92ee3a1e0af05864f17ce1
Your Name <you@example.com>
2023-06-23 12:19:41 +0200

    extrat Menu logic into component

---
+++ src/components/Menu.js
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import Button from './Button'
+
+const Menu = ({ open }) => {
+  return (
+    <nav
+      className={`flex w-[55%] transform flex-col justify-center bg-violet-100 ${
+        open ? 'translate-x-0' : '-translate-x-full'
+      } absolute top-0 left-0 z-50 mx-auto h-screen transition-transform duration-300`}
+    >
+      <div className='mx-auto flex flex-col gap-3 text-center'>
+        <Link href='/'>home</Link>
+        <Link href='contact'>contact</Link>
+        <Button />
+      </div>
+    </nav>
+  )
+}
+
+export default Menu
