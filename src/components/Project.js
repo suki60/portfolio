6bbf857206d889915b2a239292a4b20fe2881c53
suki60 <fran.altes@gmail.com>
2023-08-12 16:44:20 +0200

    Project add alt in logo's image

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,7 +4,7 @@ import Logo from './Logo'
 
 const Project = ({ project }) => (
   <div className='flex'>
-    <Logo size={60} src={project.logo} />
+    <Logo alt={`logo ${project.name}`} size={60} src={project.logo} />
     <div className='ml-6'>
       <h3 className='font-bold italic text-violet-300'>{project.name}</h3>
       <p className='font-light'>{project.description}</p>
