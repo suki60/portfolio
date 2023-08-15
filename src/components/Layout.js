7924009d001646faa13c21c1e5eeb91e217afbdb
suki60 <fran.altes@gmail.com>
2023-08-15 17:55:54 +0200

    move main to Layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,7 +6,7 @@ const Layout = ({ children }) => {
   return (
     <div>
       <Navbar />
-      {children}
+      <main>{children}</main>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,22 +9,20 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <main>
-        <div className='h-screen -mt-[var(--navbar-height)] flex justify-center items-center'>
-          <div className='flex text-xl sm:text-2xl md:text-4xl'>
-            <div className='flex flex-col-reverse md:mr-2'>
-              <h1>welcome to my</h1>
-            </div>
-            <div className='flex flex-col mx-2 px-1 md:py-1 text-center bg-violet-300 text-white rounded'>
-              <div>hello</div>
-              <div>world</div>
-            </div>
-            <div className='flex flex-col lg:ml-2'>
-              <h1>i&#39;m francesc altes</h1>
-            </div>
+      <div className='h-screen -mt-[var(--navbar-height)] flex justify-center items-center'>
+        <div className='flex text-xl sm:text-2xl md:text-4xl'>
+          <div className='flex flex-col-reverse md:mr-2'>
+            <h1>welcome to my</h1>
+          </div>
+          <div className='flex flex-col mx-2 px-1 md:py-1 text-center bg-violet-300 text-white rounded'>
+            <div>hello</div>
+            <div>world</div>
+          </div>
+          <div className='flex flex-col lg:ml-2'>
+            <h1>i&#39;m francesc altes</h1>
           </div>
         </div>
-      </main>
+      </div>
     </>
   )
 }
