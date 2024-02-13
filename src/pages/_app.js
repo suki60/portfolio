c43d810427d12560ac7b57c106e0a39b0a8e040e
fran <francesc.altes@gartner.com>
2024-02-13 17:38:30 +0100

    add google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import App from 'next/app'
+import Script from 'next/script'
 import UAParser from 'ua-parser-js'
 
 import Layout from '~/components/Layout'
@@ -6,7 +7,7 @@ import { ThemeProvider } from '~/contexts/ThemeContext'
 import { ViewportProvider } from '~/contexts/ViewportContext'
 import '~/styles/globals.css'
 
-const getSSRViewport = (userAgent) => {
+const getSSRViewport = userAgent => {
   const ua = UAParser(userAgent)
   const deviceType = ua.device.type
 
@@ -18,22 +19,32 @@ const getSSRViewport = (userAgent) => {
 }
 
 const MyApp = ({ Component, pageProps }) => (
-  <ThemeProvider>
-    <ViewportProvider ssrViewport={pageProps.ssrViewport}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ViewportProvider>
-  </ThemeProvider>
+  <>
+    {/* Google tag (gtag.js) */}
+    <Script async src='https://www.googletagmanager.com/gtag/js?id=G-2CZZ5HS9WC' />
+    <Script strategy='afterInteractive' id='gtag'>
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-2CZZ5HS9WC');
+      `}
+    </Script>
+    <ThemeProvider>
+      <ViewportProvider ssrViewport={pageProps.ssrViewport}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ViewportProvider>
+    </ThemeProvider>
+  </>
 )
 
-MyApp.getInitialProps = async (appContext) => {
+MyApp.getInitialProps = async appContext => {
   const props = await App.getInitialProps(appContext)
 
   const userAgent =
-    typeof window !== 'undefined'
-      ? window.navigator.userAgent
-      : appContext.ctx.req.headers['user-agent']
+    typeof window !== 'undefined' ? window.navigator.userAgent : appContext.ctx.req.headers['user-agent']
 
   const ssrViewport = getSSRViewport(userAgent)
   props.pageProps.ssrViewport = ssrViewport
