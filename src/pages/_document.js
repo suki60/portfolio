e7ea53b1ba8c19c3e0d6fc3ecd3d1409b7f656fc
fran <francesc.altes@gartner.com>
2024-03-01 17:12:33 +0100

    remove gtag & use gtm

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,33 +1,19 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 function Document() {
   return (
     <Html>
       <Head>
-        {/* gtag */}
-        <script async src='https://www.googletagmanager.com/gtag/js?id=G-2CZZ5HS9WC' />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-2CZZ5HS9WC');
-              `,
-          }}
-        />
-        {/* gtm */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-KSXH929M');
-            `,
-          }}
-        />
+        <Script id='gtm-script' strategy='afterInteractive'>
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KSXH929M');
+          `}
+        </Script>
       </Head>
       <body>
         <Main />
