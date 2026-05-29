132839a10ebdc0c82dcb16e307988e2166387e39
suki60 <fran.altes@gmail.com>
2026-05-30 00:07:15 +0200

    rm: project header

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -58,8 +58,6 @@ const projects = [
   },
 ]
 
-const pad = i => String(i + 1).padStart(2, '0')
-
 const Projects = () => (
   <>
     <Head>
@@ -67,27 +65,6 @@ const Projects = () => (
       <meta name='description' content='things i made' />
     </Head>
     <div className={`${inter.variable} ${mono.variable} container mt-2 pb-20`}>
-      {/* page head */}
-      <div className='flex items-end justify-between gap-5'>
-        <h1
-          className='m-0 lowercase'
-          style={{
-            fontFamily: 'var(--font-inter)',
-            fontWeight: 900,
-            fontSize: 92,
-            lineHeight: 0.88,
-            letterSpacing: '-0.045em',
-          }}
-        >
-          projects
-        </h1>
-        <div className='pb-2 text-right' style={{ fontFamily: 'var(--font-mono)', fontSize: 12, lineHeight: 1.6 }}>
-          <div>[ {pad(projects.length - 1)} entries ]</div>
-          <div className='opacity-60'>things i made</div>
-        </div>
-      </div>
-
-      {/* grid */}
       <div className='mt-11 grid grid-cols-1 gap-7 md:grid-cols-2'>
         {projects.map((project, i) => (
           <Project key={project.name} project={project} index={i} />
