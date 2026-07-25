d56b63c604f809ab293c762b7544ead88d279b51
suki60 <fran.altes@gmail.com>
2026-07-25 13:03:37 +0200

    add: project title

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -15,6 +15,8 @@ const mono = Space_Mono({
   variable: '--font-mono',
 })
 
+const pad = i => String(i + 1).padStart(2, '0')
+
 const projects = [
   {
     name: 'portfolio',
@@ -33,6 +35,25 @@ const Projects = () => (
       <meta name='description' content='things i made' />
     </Head>
     <div className={`${inter.variable} ${mono.variable} container mt-2 pb-20`}>
+      {/* page head */}
+      <div className='flex items-end justify-between gap-5'>
+        <h1
+          className='m-0 lowercase'
+          style={{
+            fontFamily: 'var(--font-inter)',
+            fontWeight: 900,
+            fontSize: 92,
+          }}
+        >
+          projects
+        </h1>
+        <div className='pb-2 text-right' style={{ fontFamily: 'var(--font-mono)', fontSize: 12, lineHeight: 1.6 }}>
+          <div>[ {pad(projects.length - 1)} entries ]</div>
+          <div className='opacity-60'>things i made</div>
+        </div>
+      </div>
+
+      {/* grid */}
       <div className='mt-11 grid grid-cols-1 gap-7 md:grid-cols-2'>
         {projects.map((project, i) => (
           <Project key={project.name} project={project} index={i} />
