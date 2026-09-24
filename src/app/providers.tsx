f28e22787e35b613ce47e54a3223a50ff24eb59e
suki60 <fran.altes@gmail.com>
2026-09-24 20:31:17 +0200

    upd: tailwind v3 -> v4

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,17 +4,13 @@ import { ReactNode } from 'react'
 
 import { ViewportProvider, getServerViewport } from '@suki60/use-viewport'
 
-import { ThemeProvider } from '~/contexts/theme-context'
-
 type ProvidersProps = {
   userAgent: string
   children: ReactNode
 }
 
 const Providers = ({ userAgent, children }: ProvidersProps) => (
-  <ThemeProvider>
-    <ViewportProvider ssrViewport={getServerViewport(userAgent)}>{children}</ViewportProvider>
-  </ThemeProvider>
+  <ViewportProvider ssrViewport={getServerViewport(userAgent)}>{children}</ViewportProvider>
 )
 
 export default Providers
